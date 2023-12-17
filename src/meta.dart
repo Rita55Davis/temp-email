c1a93f678df24b173b0ada6d5d3e30ddc1f45946
Rita Davis <154096593+Rita55Davis@users.noreply.github.com>
2023-12-18 00:15:20 +0200

    Create meta.dart

---
+++ src/meta.dart
@@ -0,0 +1,6 @@
+class Config {
+  final String cookbook;
+  late final Cookbook _cookbook;
+  final Map<String, double> doubleFilters;
+  final Map<String, int> longFilters;
+  final Map<String, String> stringFilters;
